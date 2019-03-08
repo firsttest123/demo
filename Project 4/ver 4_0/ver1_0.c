7183c3837f10008718bf6198b3a340851469ef02
firsttest123 <kalalalitha123@gmail.com>
2019-03-08 12:33:21 +0530

    Menu opeartions added

---
+++ Project 4/ver 4_0/ver1_0.c
@@ -0,0 +1,16 @@
+
+
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+
+#include <C8051F020.h>                 // SFR declarations
+#include <stdio.H>
+#include <string.H>
+#include <math.H>
+#include <INTRINS.H>
+void main()
+{
+	int project4,ver 4_0;
+}
